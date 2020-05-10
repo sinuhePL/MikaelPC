85c6fc19d0465e4f83dd022011afa455ceef10f1
sinuhePL <lawicki.karol@gmail.com>
2020-05-10 22:58:00 +0200

    Added main Menu scene with background, title and buttons

---
+++ Assets/Scripts/BlackPanelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackPanelController : MonoBehaviour
+{
+    public float transitionTime = 1.0f;
+    private Image myImage;
+    
+    private IEnumerator MakeTransparent()
+    {
+        for(float myA = 1.0f; myA > 0; myA -= 0.01f)
+        {
+            Color c = myImage.material.color;
+            c.a = myA;
+            myImage.material.color = c;
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myImage = GetComponent<Image>();
+        StartCoroutine(MakeTransparent());
+    }
+
+
+}
---
+++ Assets/Scripts/MainBackgroundController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MainBackgroundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOScale(0.305f, 120.0f);   
+    }
+}
